f1bb68b0aa948c0c23137e0ffcb48cf27a34b453
baramex <contact@baramex.me>
2022-04-21 12:54:41 +0200

    debug forbidden request

--- api.js
+++ api.js
@@ -15,6 +15,7 @@ const express = require("express");
 const router = express.Router();
 
 router.use("*", (req, res, next) => {
+    console.log(req.headers.origin, req.headers.referer, process.env.URL, req.headers["sec-fetch-site"])
     if (!(req.headers.origin || req.headers.referer || "").startsWith(process.env.URL) || req.headers["sec-fetch-site"] != "same-origin") return res.sendStatus(403);
     next();
 });
